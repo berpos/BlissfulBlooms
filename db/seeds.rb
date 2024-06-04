0339da54192270c2899ed364dd07f21f2d4335b7
bernardo campos <berposdzn@gmail.com>
2024-06-04 11:03:57 +0100

    seeding for testing and development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,24 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Destroying DataBase"
+
+Log.destroy_all
+Plant.destroy_all
+Location.destroy_all
+
+puts "Creating new seeds"
+
+10.times do
+  Location.create!(
+    user_id: 2,
+    name: Faker::Name.first_name
+  )
+end
+
+10.times do
+  Plant.create!(
+    location_id: Location.last.id,
+    name: Faker::Name.first_name,
+    state: %i[Very bad bad good very good].sample,
+    updated_at: "today"
+  )
+end
+puts 'New databse created'
