db7f5e376197c88416f504487ea2ef48cebc873b
Marco Gomes <marco6_1@hotmail.com>
2024-06-04 18:03:28 +0100

    plants index

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,11 +1,6 @@
 class LocationsController < ApplicationController
-<<<<<<< HEAD
   def show
     @location = Location.find(params[:id])
    # @plants = @location.plants
-=======
-  def index
-    @locations = Location.all
->>>>>>> master
   end
 end
