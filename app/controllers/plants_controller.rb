9d0a672ad30d52568666da98c784712bf98fae6a
Manuel Bessa <manelforbesbessa@gmail.com>
2024-06-05 12:38:05 +0100

    plants-index

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,16 @@
 class PlantsController < ApplicationController
+
+  def index
+    @plants = Plant.all
+  end
+
+  private
+
+  def set_plant
+    @plant = Plant.find(params[:id])
+  end
+
+  def plant_params
+    params.require(:plant).permit(:name, :state, :updated_at)
+  end
 end
