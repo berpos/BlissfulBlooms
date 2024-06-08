d2981513edca19bd6f9b33c01c52e9a20d5c0ec0
bernardo campos <berposdzn@gmail.com>
2024-06-08 11:53:41 +0100

    default state and level

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -5,4 +5,16 @@ class Plant < ApplicationRecord
   validates :categories, inclusion: { in: ['person', 'moment'] }
   validates :name, :categories, presence: true
   accepts_nested_attributes_for :logs
+
+  before_validation :set_default_state, :set_default_level, on: :create
+
+  private
+
+  def set_default_state
+    self.state ||= 'healthy'
+  end
+
+  def set_default_level
+    self.level ||= 0
+  end
 end
