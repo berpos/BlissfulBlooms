eac47b025404ac52252ec48ce3f7e389e0c9e3ce
bernardo campos <berposdzn@gmail.com>
2024-06-12 11:41:49 +0100

    button javascript bug fix

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,8 +7,6 @@ import { application } from "controllers/application";
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
-import ActionButtonsController from "./action_buttons_controller";
-Stimulus.register("disable-button", ActionButtonsController)
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
